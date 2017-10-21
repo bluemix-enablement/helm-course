74467c2c7577363a9f73ba9d90d03c9d9fef22ef
Budi Darmawan <vbudi@us.ibm.com>
2017-10-20 23:08:47 -0400

    mod mysql

--- script/populateMysql.sh
+++ script/populateMysql.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
    echo "Populate Mysql"
+   apt-get install -y mysql-client
+
+while !(mysql -udbuser -pPass4dbUs3R --host mysql-lightblue-service --port 3306 --ssl-mode=REQUIRED <load-data.sql)
+do
+  printf "Waiting for MySQL to fully initialize\n\n"
+  sleep 1
+    echo "trying to load data again"
+done
+
 
 exit
